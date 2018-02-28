10d91756bd82a6033f47c063cbe16430885009da
Brian A Morales <bombibills@gmail.com>
2018-02-28 20:15:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const app = "I don't do much."
 function Arrays(){
   window.kittens = ['Milo', 'Otis', 'Garfield'];
 }
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+  Arrays();
+}
