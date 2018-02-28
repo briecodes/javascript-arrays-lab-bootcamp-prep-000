c2467cbffea4e622643181192c33d7aa494a8b1a
Brian A Morales <bombibills@gmail.com>
2018-02-28 20:20:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function destructivelyPrependKitten(name){
   kittens.unshift(name);
   return kittens;
 }
+
+function destructivelyRemoveLastKitten(){
+  Arrays();
+  kittens.shift();
+  return kittens;
+}
